e66582a23455d780061c68a1daf413c01f16948d
Adam Labbe <adamlabbe@gmail.com>
2017-10-10 16:32:30 -0400

    Remove list node before deleting attached data

--- dyn_export.c
+++ dyn_export.c
@@ -28,250 +28,251 @@ static LIST_HEAD(dev_list);
 static DEFINE_MUTEX(sysfs_lock);
 
 struct dyn_export_data {
-    struct list_head list;
-    int number;
-    int thing1;
-    int thing2;
-    /* other meta data */
+	struct list_head list;
+	int number;
+	int thing1;
+	int thing2;
+	/* other meta data */
 };
 
 static int match_export(struct device *dev, const void *desc)
 {
-    const long number = (const long)desc;
-    struct dyn_export_data *data = dev_get_drvdata(dev);
-    return data->number == number;
+	const long number = (const long)desc;
+	struct dyn_export_data *data = dev_get_drvdata(dev);
+	return data->number == number;
 }
 
 static ssize_t export_store(struct class *class,
-                            struct class_attribute *attr,
-                            const char *buf, size_t len);
+							struct class_attribute *attr,
+							const char *buf, size_t len);
 static ssize_t unexport_store(struct class *class,
-                            struct class_attribute *attr,
-                            const char *buf, size_t len);
+							struct class_attribute *attr,
+							const char *buf, size_t len);
 
 static struct dyn_export_data * dyn_alloc(long number);
 static ssize_t dyn_free(long number);
 
 static struct class_attribute dyn_export_class_attrs[] = {
-    __ATTR_WO(export),
-    __ATTR_WO(unexport),
-    __ATTR_NULL,
+	__ATTR_WO(export),
+	__ATTR_WO(unexport),
+	__ATTR_NULL,
 };
 
 static struct class dyn_export_class = {
-    .name = CLASS_NAME,
-    .owner = THIS_MODULE,
-    .class_attrs = dyn_export_class_attrs,
+	.name = CLASS_NAME,
+	.owner = THIS_MODULE,
+	.class_attrs = dyn_export_class_attrs,
 };
 
 /* These are per-device attributes, and show up in:
  * /sys/class/dyn_export/dynN/ after /sys/class/dyn_export/export is used.
  */
 static ssize_t thing1_show(struct device *dev,
-                         struct device_attribute *attr, char *buf)
+						 struct device_attribute *attr, char *buf)
 {
-    struct dyn_export_data *data = dev_get_drvdata(dev);
-    return scnprintf(buf, PAGE_SIZE, "%d", data->thing1);
+	struct dyn_export_data *data = dev_get_drvdata(dev);
+	return scnprintf(buf, PAGE_SIZE, "%d", data->thing1);
 }
 
 static ssize_t thing1_store(struct device *dev,
-                          struct device_attribute *attr, const char *buf, size_t size)
+						  struct device_attribute *attr, const char *buf, size_t size)
 {
-    long number;
-    int status;
-    struct dyn_export_data *data = dev_get_drvdata(dev);
+	long number;
+	int status;
+	struct dyn_export_data *data = dev_get_drvdata(dev);
 
-    status = kstrtol(buf, 0, &number);
-    if (status < 0) {
-        err("could not parse input as a long, status=%d", status);
-        return -EINVAL;
-    }
-    data->thing1 = number;
-    return size;
+	status = kstrtol(buf, 0, &number);
+	if (status < 0) {
+		err("could not parse input as a long, status=%d", status);
+		return -EINVAL;
+	}
+	data->thing1 = number;
+	return size;
 }
 static DEVICE_ATTR_RW(thing1);
 
 static ssize_t thing2_show(struct device *dev,
-                         struct device_attribute *attr, char *buf)
+						 struct device_attribute *attr, char *buf)
 {
-    struct dyn_export_data *data = dev_get_drvdata(dev);
-    return scnprintf(buf, PAGE_SIZE, "%d", data->thing2);
+	struct dyn_export_data *data = dev_get_drvdata(dev);
+	return scnprintf(buf, PAGE_SIZE, "%d", data->thing2);
 }
 
 static ssize_t thing2_store(struct device *dev,
-                          struct device_attribute *attr, const char *buf, size_t size)
+						  struct device_attribute *attr, const char *buf, size_t size)
 {
-    long number;
-    int status;
-    struct dyn_export_data *data = dev_get_drvdata(dev);
+	long number;
+	int status;
+	struct dyn_export_data *data = dev_get_drvdata(dev);
 
-    status = kstrtol(buf, 0, &number);
-    if (status < 0) {
-        err("could not parse input as a long, status=%d", status);
-        return -EINVAL;
-    }
-    data->thing2 = number;
-    return size;
+	status = kstrtol(buf, 0, &number);
+	if (status < 0) {
+		err("could not parse input as a long, status=%d", status);
+		return -EINVAL;
+	}
+	data->thing2 = number;
+	return size;
 }
 static DEVICE_ATTR_RW(thing2);
 
 static struct attribute *dyn_attrs[] = {
-    &dev_attr_thing1.attr,
-    &dev_attr_thing2.attr,
-    NULL,
+	&dev_attr_thing1.attr,
+	&dev_attr_thing2.attr,
+	NULL,
 };
 
 static const struct attribute_group dyn_group = {
-    .attrs = dyn_attrs,
+	.attrs = dyn_attrs,
 };
 
 static const struct attribute_group *dyn_groups[] = {
-    &dyn_group,
-    NULL
+	&dyn_group,
+	NULL
 };
 
 /* top-level, "class" attributes.
  * Only one set of these ever exist, in /sys/class/dyn_export/{export,unexport}
  */
 static ssize_t export_store(struct class *class,
-                            struct class_attribute *attr,
-                            const char *buf, size_t len)
+							struct class_attribute *attr,
+							const char *buf, size_t len)
 {
-    long number;
-    struct dyn_export_data *data;
-    int status;
-    struct device *dev;
+	long number;
+	struct dyn_export_data *data;
+	int status;
+	struct device *dev;
 
-    mutex_lock(&sysfs_lock);
-    status = kstrtol(buf, 0, &number);
-    if (status < 0) {
-        err("could not parse input as a long, status=%d", status);
-        goto err_parse_input;
-    }
+	mutex_lock(&sysfs_lock);
+	status = kstrtol(buf, 0, &number);
+	if (status < 0) {
+		err("could not parse input as a long, status=%d", status);
+		goto err_parse_input;
+	}
 
-    data = dyn_alloc(number);
-    if (!data) {
-        goto err_alloc_data;
-    }
+	data = dyn_alloc(number);
+	if (!data) {
+		goto err_alloc_data;
+	}
 
-    dev = device_create_with_groups(&dyn_export_class, NULL,
-                                    MKDEV(0,0), data, dyn_groups,
-                                    "dyn%ld", number);
-    if (IS_ERR(dev)) {
-        err("Could not create device for dyn%ld", number);
-        status = PTR_ERR(dev);
-        goto err_dev_create;
-    }
+	dev = device_create_with_groups(&dyn_export_class, NULL,
+									MKDEV(0,0), data, dyn_groups,
+									"dyn%ld", number);
+	if (IS_ERR(dev)) {
+		err("Could not create device for dyn%ld", number);
+		status = PTR_ERR(dev);
+		goto err_dev_create;
+	}
 
-    list_add(&data->list, &dev_list);
+	list_add(&data->list, &dev_list);
 
-    mutex_unlock(&sysfs_lock);
+	mutex_unlock(&sysfs_lock);
 
-    return len;
+	return len;
 
 err_dev_create:
-    kfree(data);
+	kfree(data);
 err_alloc_data:
 err_parse_input:
-    mutex_unlock(&sysfs_lock);
-    return status;
+	mutex_unlock(&sysfs_lock);
+	return status;
 }
 
 static ssize_t unexport_store(struct class *class,
-                            struct class_attribute *attr,
-                            const char *buf, size_t len)
+							struct class_attribute *attr,
+							const char *buf, size_t len)
 {
-    long number;
-    int status;
+	long number;
+	int status;
 
-    mutex_lock(&sysfs_lock);
-    status = kstrtol(buf, 0, &number);
-    if (status < 0) {
-        err("could not parse input as a long, status=%d", status);
-        goto err_parse_input;
-    }
+	mutex_lock(&sysfs_lock);
+	status = kstrtol(buf, 0, &number);
+	if (status < 0) {
+		err("could not parse input as a long, status=%d", status);
+		goto err_parse_input;
+	}
 
-    status = dyn_free(number);
-    if (status < 0) {
-        goto err_dyn_free_error;
-    }
+	list_del(&data->list);
+	status = dyn_free(number);
+	if (status < 0) {
+		goto err_dyn_free_error;
+	}
 
-    mutex_unlock(&sysfs_lock);
-    return len;
+	mutex_unlock(&sysfs_lock);
+	return len;
 
 err_dyn_free_error:
 err_parse_input:
-    mutex_unlock(&sysfs_lock);
-    return status;
+	mutex_unlock(&sysfs_lock);
+	return status;
 }
 
 static struct dyn_export_data * dyn_alloc(long number)
 {
-    struct dyn_export_data *data;
-    int status;
+	struct dyn_export_data *data;
+	int status;
 
-    data = kzalloc(sizeof(struct dyn_export_data), GFP_KERNEL);
-    if (!data) {
-        err("could not allocate memory");
-        status = -ENOMEM;
-        goto err_alloc_data;
-    }
+	data = kzalloc(sizeof(struct dyn_export_data), GFP_KERNEL);
+	if (!data) {
+		err("could not allocate memory");
+		status = -ENOMEM;
+		goto err_alloc_data;
+	}
 
-    data->number = number;
-    data->thing1 = data->thing2 = 0;
-    return data;
+	data->number = number;
+	data->thing1 = data->thing2 = 0;
+	INIT_LIST_HEAD(&data->list);
+	return data;
 
 err_alloc_data:
-    return NULL;
+	return NULL;
 }
 
 static ssize_t dyn_free(long number)
 {
-    int status;
-    struct dyn_export_data *data;
-    struct device *dev;
+	int status;
+	struct dyn_export_data *data;
+	struct device *dev;
 
-    dev = class_find_device(&dyn_export_class, NULL, (const void *)number, match_export);
-    if (!dev) {
-        err("'%ld' does not appear to be exported", number);
-        status = -EINVAL;
-        goto err_not_exported;
-    }
+	dev = class_find_device(&dyn_export_class, NULL, (const void *)number, match_export);
+	if (!dev) {
+		err("'%ld' does not appear to be exported", number);
+		status = -EINVAL;
+		goto err_not_exported;
+	}
 
-    data = dev_get_drvdata(dev);
+	data = dev_get_drvdata(dev);
 
-    device_unregister(dev);
-    put_device(dev);
-    kfree(data);
-    return 0;
+
+	device_unregister(dev);
+	put_device(dev);
+	kfree(data);
+	return 0;
 
 err_not_exported:
-    return status;
+	return status;
 }
-
 
 static int __init dyn_export_module_init(void)
 {
-    int retval;
+	int retval;
 
-    retval = class_register(&dyn_export_class);
-    if (retval < 0) {
-        err("failed to register device class '%s'\n", CLASS_NAME);
-        return -EINVAL;
-    }
+	retval = class_register(&dyn_export_class);
+	if (retval < 0) {
+		err("failed to register device class '%s'\n", CLASS_NAME);
+		return -EINVAL;
+	}
 
-    return 0;
+	return 0;
 }
 
 static void __exit dyn_export_module_exit(void)
 {
-    struct list_head *item;
-    struct dyn_export_data *data;
-    list_for_each(item, &dev_list) {
-        data = list_entry(item, struct dyn_export_data, list);
-        dyn_free(data->number);
-    }
-    class_destroy(&dyn_export_class);
+	struct dyn_export_data *data, *tmp;
+	list_for_each_entry_safe(data, tmp, &dev_list, list) {
+		list_del(&data->list);
+		dyn_free(data->number);
+	}
+	class_destroy(&dyn_export_class);
 }
 
 module_init(dyn_export_module_init);
